faa0573592735281d417b2e6abd2906f4035d64c
Glen Robson <glen.robson@gmail.com>
2017-10-07 00:01:45 +0100

    Adding redirects for root and missing slash.

--- tests/TestPrezi3.py
+++ tests/TestPrezi3.py
@@ -17,10 +17,29 @@ class TestPrezi3(unittest.TestCase):
         code = response.status_code
         self.assertEqual(code, 200, 'Failed to get 200 from host %s due to %s' % (host, code))
 
+    def redirectCheck(self, url, target):
+        code = 0
+        response = requests.get(url, allow_redirects=False, headers={'host': 'prezi3.iiif.io'})
+        code = response.status_code
+        location = url
+        if 'Location' in response.headers:
+            location = response.headers['Location']
+        self.assertEqual(code, 302, 'Failed to get redirected to %s due to %s' % (url, code))
+        self.assertEqual(location, target, 'Failed to redirect to the correct place. Expected %s but got %s' % (target, location))
+
     def test_prezi3(self):
         url = '%s/%s' % (self.baseurl, 'api/presentation/3.0/')
         self.checkOK(url, 'prezi3.iiif.io')
 
+    def test_apiredirect(self):
+        url = '%s/%s' % (self.baseurl, '')
+        target = 'http://prezi3.iiif.io/api/index.html'
+        self.redirectCheck(url, target)
+
+    def test_presentationMissingslash(self):
+        url = '%s/%s' % (self.baseurl, 'api/presentation/3.0')
+        target = 'http://prezi3.iiif.io/api/presentation/3.0/'
+        self.redirectCheck(url, target)
 
 if __name__ == '__main__':
     baseurl = 'http://localhost:5000'
