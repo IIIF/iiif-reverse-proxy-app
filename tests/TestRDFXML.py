037de098cbdc55c5e868070e7501e05dec20afd8
Glen Robson <glen.robson@gmail.com>
2018-08-16 00:25:02 +0100

    Adding tests for Image Ontology forward and adding forward.
    Also adding code to hide hacking attempts from the log.

--- tests/TestRDFXML.py
+++ tests/TestRDFXML.py
@@ -3,6 +3,12 @@ import unittest
 import os
 import sys
 import requests
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib2 import urlopen
+from urllib import request
+
 
 class TestRDFXML(unittest.TestCase):
     baseurl = ''
@@ -14,6 +20,17 @@ class TestRDFXML(unittest.TestCase):
         response=urlopen(url)
         header=response.info().get('Content-Type')
         self.assertEqual(header, 'application/rdf+xml', 'ontology.xml returned Content-Type %s not application/rdf+xml.' % header)
+
+    def test_jsonldmimetype(self):
+        url = '%s/%s' % (self.baseurl, 'api/image/2#')
+        opener = request.build_opener()
+        opener.addheaders = [('Accept', 'application/rdf+xml')]
+        response=opener.open(url)
+        location=response.geturl()
+        self.assertEqual(location, 'https://iiif.io/api/image/2/ontology.xml', 'Failed to redirect with accept')
+
+        response=urlopen(url)
+        self.assertEqual(response.geturl(), 'https://iiif.io/api/image/2.1/', 'Failed to correctly redirect without accept')
 
 if __name__ == '__main__':
     baseurl = 'http://localhost:5000'
