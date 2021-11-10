cede0979f9ac53befd9a3f620b0b2da748a01fc3
Glen Robson <glen.robson@gmail.com>
2021-11-10 11:03:42 +0000

    Adding the new website

--- tests/TestValidators.py
+++ tests/TestValidators.py
@@ -3,30 +3,45 @@ import unittest
 import os
 from urllib.request import urlopen
 from urllib import request
+import requests
 
 class TestValidators(unittest.TestCase):
-	baseurl = ''
-	def setUp(self):
-		self.baseurl = os.environ["baseurl"]
-	def test_website(self):
-		url = '%s/%s' % (self.baseurl, 'api/image/validator/service/list_tests')
-		code = 0
-		try:
-			response=urlopen(url)
-			code = response.code
-		except request.HTTPError as error:	
-			code = error.code
-		self.assertEqual(code, 200, 'Problem retriving the image validator got: %s from %s' % (code, url))
+    baseurl = ''
+    def setUp(self):
+        self.baseurl = os.environ["baseurl"]
+
+    def test_imagevalidator(self):
+        url = '%s/%s' % (self.baseurl, 'api/image/validator/service/list_tests')
+        code = 0
+        try:
+            response = requests.get(url, allow_redirects=False)
+            code = response.status_code
+        except request.HTTPError as error:	
+            code = error.code
+
+        self.assertEqual(code, 302, 'Problem retriving the image validator got: %s from %s' % (code, url))
+
+        if 'Location' in response.headers:
+            location = response.headers['Location']
 		
-	def test_api(self):
-		url = '%s/%s' % (self.baseurl, 'api/presentation/validator/service')
-		code = 0
-		try:
-			response=urlopen(url)
-			code = response.code
-		except request.HTTPError as error:	
-			code = error.code
-		self.assertEqual(code,200, 'Problem retriving the presentation validator got: %s from %s' % (code, url))
+        self.assertEqual(location, 'https://image-validator.iiif.io/list_tests', "Redirect target incorrect")
+        ## TODO test webp
+
+    def test_api(self):
+        url = '%s/%s' % (self.baseurl, 'api/presentation/validator/service/validate')
+        code = 0
+        try:
+            response = requests.get(url, allow_redirects=False)
+            code = response.status_code
+        except request.HTTPError as error:	
+            code = error.code
+
+        self.assertEqual(code,302, 'Problem retriving the presentation validator got: %s from %s' % (code, url))
+
+        if 'Location' in response.headers:
+            location = response.headers['Location']
+
+        self.assertEqual(location, 'https://presentation-validator.iiif.io/validate', "Redirect target incorrect")
 
 if __name__ == '__main__':
 	unittest.main()
